b34fa59e08ac72f38a717ddcd5b45a39bd345f0c
Bruno Antunes <sardaukar.siet@gmail.com>
2018-11-04 13:11:15 +0000

    Cleanup

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React, { Component } from 'react'
 import classNames from 'classnames'
 import './App.css'
 
-class App extends Component {
+export default class App extends Component {
   constructor(props) {
     super(props)
 
     const time = this.getTimeString()
-    this.state = {
-      time: time
-    }
+    this.state = {time}
   }
 
   componentDidMount() {
@@ -22,19 +20,26 @@ class App extends Component {
 
   tick() {
     const time = this.getTimeString()
-    this.setState((state, props) => {return {time}})
+    this.setState((_state, _props) => {return {time}})
   }
 
   getTimeString() {
-    return new Date().toISOString().substr(11, 8).replace(/:/g , "")
+    return new Date().toISOString().substr(11, 8).replace(/:/g , '')
   }
 
   padLeft(nr, n, str){
-    return Array(n-String(nr).length+1).join(str||'0')+nr
+    return Array(n - String(nr).length + 1).join(str || '0') + nr
   }
 
   toBCD(value) {
     return this.padLeft(Number(value).toString(2), 4).split("")
+  }
+
+  classesFor(value) {
+    return {
+      'on': value === '1',
+      'off': value === '0'
+    }
   }
 
   render() {
@@ -57,178 +62,116 @@ class App extends Component {
         <header className="App-header">
           <div className="wrapper">
 
-            <div className="box none" /> {/* hours[0][0] */}
+            <div className="box none" />
             <div className={
               classNames(
                 'box',
-                {
-                  'on': hours[1][0] === '1',
-                  'off': hours[1][0] === '0'
-                }
-              )} />  {/* hours[1][0] */}
-            <div className="box none" /> {/* minutes[0][0] */}
+                this.classesFor(hours[1][0])
+              )} />
+            <div className="box none" />
             <div className={
               classNames(
                 'box',
-                {
-                  'on': minutes[1][0] === '1',
-                  'off': minutes[1][0] === '0'
-                }
-              )} />  {/* minutes[1][0] */}
-            <div className="box none" /> {/* seconds[0][0] */}
+                this.classesFor(minutes[1][0])
+              )} />
+            <div className="box none" />
             <div className={
               classNames(
                 'box',
-                {
-                  'on': seconds[1][0] === '1',
-                  'off': seconds[1][0] === '0'
-                }
-              )} />  {/* seconds[1][0] */}
+                this.classesFor(seconds[1][0])
+              )} />
 
-            <div className="box none" /> {/* hours[0][1] */}
+            <div className="box none" />
             <div className={
               classNames(
                 'box',
-                {
-                  'on': hours[1][1] === '1',
-                  'off': hours[1][1] === '0'
-                }
-              )} />  {/* hours[1][1] */}
+                this.classesFor(hours[1][1])
+              )} />
             <div className={
               classNames(
                 'box',
-                {
-                  'on': minutes[0][1] === '1',
-                  'off': minutes[0][1] === '0'
-                }
-              )} />  {/* minutes[0][1] */}
+                this.classesFor(minutes[0][1])
+              )} />
             <div className={
               classNames(
                 'box',
-                {
-                  'on': minutes[1][1] === '1',
-                  'off': minutes[1][1] === '0'
-                }
-              )} />  {/* minutes[1][1] */}
+                this.classesFor(minutes[1][1])
+              )} />
               <div className={
               classNames(
                 'box',
-                {
-                  'on': seconds[0][1] === '1',
-                  'off': seconds[0][1] === '0'
-                }
-              )} />  {/* seconds[0][1] */}
+                this.classesFor(seconds[0][1])
+              )} />
               <div className={
               classNames(
                 'box',
-                {
-                  'on': seconds[1][1] === '1',
-                  'off': seconds[1][1] === '0'
-                }
-              )} />  {/* seconds[1][1] */}
+                this.classesFor(seconds[1][1])
+              )} />
 
             <div className={
               classNames(
                 'box',
-                {
-                  'on': hours[0][2] === '1',
-                  'off': hours[0][2] === '0'
-                }
-              )} />  {/* hours[0][2] */}
+                this.classesFor(hours[0][2])
+              )} />
             <div className={
               classNames(
                 'box',
-                {
-                  'on': hours[1][2] === '1',
-                  'off': hours[1][2] === '0'
-                }
-              )} />  {/* hours[1][2] */}
+                this.classesFor(hours[1][2])
+              )} />
             <div className={
               classNames(
                 'box',
-                {
-                  'on': minutes[0][2] === '1',
-                  'off': minutes[0][2] === '0'
-                }
-              )} />  {/* minutes[0][2] */}
+                this.classesFor(minutes[0][2])
+              )} />
             <div className={
               classNames(
                 'box',
-                {
-                  'on': minutes[1][2] === '1',
-                  'off': minutes[1][2] === '0'
-                }
-              )} />  {/* minutes[1][2] */}
+                this.classesFor(minutes[1][2])
+              )} />
             <div className={
               classNames(
                 'box',
-                {
-                  'on': seconds[0][2] === '1',
-                  'off': seconds[0][2] === '0'
-                }
-              )} />  {/* seconds[0][2] */}
+                this.classesFor(seconds[0][2])
+              )} />
             <div className={
               classNames(
                 'box',
-                {
-                  'on': seconds[1][2] === '1',
-                  'off': seconds[1][2] === '0'
-                }
-              )} />  {/* seconds[1][2] */}
+                this.classesFor(seconds[1][2])
+              )} />
 
             <div className={
               classNames(
                 'box',
-                {
-                  'on': hours[0][3] === '1',
-                  'off': hours[0][3] === '0'
-                }
-              )} />  {/* hours[0][3] */}
+                this.classesFor(hours[0][3])
+              )} />
             <div className={
               classNames(
                 'box',
-                {
-                  'on': hours[1][3] === '1',
-                  'off': hours[1][3] === '0'
-                }
-              )} />  {/* hours[1][3] */}
+                this.classesFor(hours[1][3])
+              )} />
             <div className={
               classNames(
                 'box',
-                {
-                  'on': minutes[0][3] === '1',
-                  'off': minutes[0][3] === '0'
-                }
-              )} />  {/* minutes[0][3] */}
+                this.classesFor(minutes[0][3])
+              )} />
             <div className={
               classNames(
                 'box',
-                {
-                  'on': minutes[1][3] === '1',
-                  'off': minutes[1][3] === '0'
-                }
-              )} />  {/* minutes[1][3] */}
+                this.classesFor(minutes[1][3])
+              )} />
             <div className={
               classNames(
                 'box',
-                {
-                  'on': seconds[0][3] === '1',
-                  'off': seconds[0][3] === '0'
-                }
-              )} />  {/* seconds[0][3] */}
+                this.classesFor(seconds[0][3])
+              )} />
             <div className={
               classNames(
                 'box',
-                {
-                  'on': seconds[1][3] === '1',
-                  'off': seconds[1][3] === '0'
-                }
-              )} />  {/* seconds[1][3] */}
+                this.classesFor(seconds[1][3])
+              )} />
           </div>
         </header>
       </div>
     )
   }
 }
-
-export default App
