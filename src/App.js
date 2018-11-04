6d9354c8250cda63225835bc47ed3035542fa098
Bruno Antunes <sardaukar.siet@gmail.com>
2018-11-04 12:29:04 +0000

    better state setting

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css'
 
 class App extends Component {
   componentDidMount() {
-    this.setState({time: new Date()})
+    const time = this.getTimeString()
+    this.setState((state, props) => {return {time}})
 
     this.interval = setInterval(() => this.tick(), 1000)
   }
@@ -12,7 +13,12 @@ class App extends Component {
   }
 
   tick() {
-    this.setState({time: new Date()})
+    const time = this.getTimeString()
+    this.setState((state, props) => {return {time}})
+  }
+
+  getTimeString() {
+    return new Date().toISOString().substr(11, 8).replace(/:/g , "")
   }
 
   render() {
