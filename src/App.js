adc6ccdb95d0d0ec77266390f17ec3afd1cd1213
Bruno Antunes <sardaukar.siet@gmail.com>
2018-11-04 12:44:14 +0000

    binaryfication of state value

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import React, { Component } from 'react'
 import './App.css'
 
 class App extends Component {
-  componentDidMount() {
-    const time = this.getTimeString()
-    this.setState((state, props) => {return {time}})
+  constructor(props) {
+    super(props)
 
+    const time = this.getTimeString()
+    this.state = {
+      time: time
+    }
+  }
+
+  componentDidMount() {
     this.interval = setInterval(() => this.tick(), 1000)
   }
+
   componentWillUnmount() {
     clearInterval(this.interval)
   }
@@ -18,10 +25,27 @@ class App extends Component {
   }
 
   getTimeString() {
-    return new Date().toISOString().substr(11, 8).replace(/:/g , "")
+    return new Date().toISOString().substr(11, 5).replace(/:/g , "")
+  }
+
+  padLeft(nr, n, str){
+    return Array(n-String(nr).length+1).join(str||'0')+nr;
+  }
+
+  toBCD(value) {
+    return this.padLeft(Number(value).toString(2), 4)
   }
 
   render() {
+    const hours = [
+      this.toBCD(this.state.time[0]),
+      this.toBCD(this.state.time[1])
+    ]
+    const minutes = [
+      this.toBCD(this.state.time[2]),
+      this.toBCD(this.state.time[3])
+    ]
+
     return (
       <div className="App">
         <header className="App-header">
